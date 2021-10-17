76f804814fb39bd6a6786e42bc3f3cb216f76c7a
Mark Zadel <mzadel@users.noreply.github.com>
2021-10-17 13:45:34 +0200

    Make a circle with tick marks

--- tidaldiagram.hs
+++ tidaldiagram.hs
@@ -16,12 +16,21 @@ Verify that radiusOfUnitCircumfrenceCircle gives us a circle of circumfrence
 1.0001402927134473
 --}
 
+circleWithTicks :: Diagram B
+circleWithTicks = circle radiusOfUnitCircumfrenceCircle <> mconcat tickMarks
+    where
+        tickMarkSize = 0.1 * radiusOfUnitCircumfrenceCircle
+        topPoint = p2 (0, radiusOfUnitCircumfrenceCircle)
+        lineSeg = vrule tickMarkSize # moveTo topPoint
+        tickMarkPositions = map (/ 7) [0..6] :: [Double]
+        tickMarks = [ rotateBy r $ lineSeg | r <- tickMarkPositions]
+
 myCircle :: Diagram B
 myCircle = circle radiusOfUnitCircumfrenceCircle # fc green
 
 outputScaling = 1000 :: Double
 
-outputDiagram = myCircle
+outputDiagram = circleWithTicks
 
 main :: IO ()
 main = mainWith (outputDiagram # scale outputScaling)
