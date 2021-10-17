7f9eb0c4851f8448edbad9ffe391f9964e55e695
Mark Zadel <mzadel@users.noreply.github.com>
2021-10-17 13:48:05 +0200

    Add alias theRadius

--- tidaldiagram.hs
+++ tidaldiagram.hs
@@ -6,6 +6,7 @@ import Diagrams.Prelude
 import Diagrams.Backend.SVG.CmdLine
 
 radiusOfUnitCircumfrenceCircle = 1.0 / (2.0 * pi) :: Double
+theRadius = radiusOfUnitCircumfrenceCircle
 
 {--
 Verify that radiusOfUnitCircumfrenceCircle gives us a circle of circumfrence
@@ -17,17 +18,17 @@ Verify that radiusOfUnitCircumfrenceCircle gives us a circle of circumfrence
 --}
 
 circleWithTicks :: Diagram B
-circleWithTicks = circle radiusOfUnitCircumfrenceCircle <> mconcat tickMarks
+circleWithTicks = circle theRadius <> mconcat tickMarks
     where
         numParts = 7
-        tickMarkSize = 0.1 * radiusOfUnitCircumfrenceCircle
-        topPoint = p2 (0, radiusOfUnitCircumfrenceCircle)
+        tickMarkSize = 0.1 * theRadius
+        topPoint = p2 (0, theRadius)
         lineSeg = vrule tickMarkSize # moveTo topPoint
         tickMarkPositions = map (/ numParts) [0..(numParts-1)] :: [Double]
         tickMarks = [ rotateBy r $ lineSeg | r <- tickMarkPositions]
 
 myCircle :: Diagram B
-myCircle = circle radiusOfUnitCircumfrenceCircle # fc green
+myCircle = circle theRadius # fc green
 
 outputScaling = 1000 :: Double
 
