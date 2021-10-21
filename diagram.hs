fa13ec2739f8162f0d7a20cc10c3481d97107123
Mark Zadel <mzadel@users.noreply.github.com>
2021-10-21 15:33:24 +0200

    Add change the colours and the events for the second diagram

--- diagram.hs
+++ diagram.hs
@@ -7,17 +7,23 @@ import Data.Map (Map,fromList)
 
 outputScaling = 1000 :: Double
 
-numTicks = 7
-
-colourTable :: Map String Int
-colourTable = fromList [
+colourTable1 :: Map String Int
+colourTable1 = fromList [
      ("a", 2)
     ,("b", 2)]
 
-outputDiagram :: Diagram B
-outputDiagram = patternDiagram [("a", 1/7, 2/7), ("b", 3/7, 5/7)] numTicks colourTable
+colourTable2 :: Map String Int
+colourTable2 = fromList [
+     ("a", 3)
+    ,("b", 5)]
 
-content = outputDiagram === (outputDiagram # scale 0.5)
+outputDiagram1 :: Diagram B
+outputDiagram1 = patternDiagram [("a", 1/7, 2/7), ("b", 3/7, 5/7)] 7 colourTable1
+
+outputDiagram2 :: Diagram B
+outputDiagram2 = patternDiagram [("a", 0, 3/8), ("b", 2/4, 3/4)] 4 colourTable2
+
+content = outputDiagram1 === (outputDiagram2 # scale 0.5)
 
 main :: IO ()
 main = mainWith $ content # frame 0.05 # scale outputScaling
