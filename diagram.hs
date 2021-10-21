efb100645957d59486fb6b41e6c7538bc138da9c
Mark Zadel <mzadel@users.noreply.github.com>
2021-10-21 16:40:10 +0200

    Just use a single colour table

--- diagram.hs
+++ diagram.hs
@@ -7,13 +7,8 @@ import qualified Sound.Tidal.Context as T
 
 outputScaling = 1000 :: Double
 
-colourTable1 :: Map String Int
-colourTable1 = fromList [
-     ("a", 2)
-    ,("b", 2)]
-
-colourTable2 :: Map String Int
-colourTable2 = fromList [
+colourTable :: Map String Int
+colourTable = fromList [
      ("a", 0)
     ,("b", 2)]
 
@@ -21,10 +16,10 @@ pat1 = T.s $ T.parseBP_E "a a b"
 pat2 = T.s $ T.parseBP_E "a b b"
 
 outputDiagram1 :: Diagram B
-outputDiagram1 = patternDiagram pat1 7 colourTable1
+outputDiagram1 = patternDiagram pat1 7 colourTable
 
 outputDiagram2 :: Diagram B
-outputDiagram2 = patternDiagram pat2 4 colourTable2
+outputDiagram2 = patternDiagram pat2 4 colourTable
 
 content = outputDiagram1 === (outputDiagram2 # scale 0.5)
 
