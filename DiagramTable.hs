30298e5af6f63dde3f33a34d56f450d74295145e
Mark Zadel <mzadel@users.noreply.github.com>
2021-10-22 15:18:29 +0200

    Add euclideanrhythm diagram

--- DiagramTable.hs
+++ DiagramTable.hs
@@ -66,6 +66,8 @@ diagramTableLinear = [
     ,(patternDiagramLinear (T.s $ T.parseBP_E "{a b c d e}%7") 7 2 colourTable) # frame 0.05 # scale outputScaling)
     ,("anglebrackets"
     ,(patternDiagramLinear (T.s $ T.parseBP_E "<a b c> d <e f>") 3 4 colourTable) # frame 0.05 # scale outputScaling)
+    ,("euclideanrhythm"
+    ,(patternDiagramLinear (T.s $ T.parseBP_E "a(3,7)") 7 2 colourTable) # frame 0.05 # scale outputScaling)
     ]
 
 diagramListForMainWith :: [(String, Diagram B)]
