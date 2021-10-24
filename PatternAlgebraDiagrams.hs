1f5d11b692952c096d4972c5d5713280e5b83eea
Mark Zadel <mzadel@users.noreply.github.com>
2021-10-24 20:09:08 +0200

    Factor out patternAlgebraDiagram

--- PatternAlgebraDiagrams.hs
+++ PatternAlgebraDiagrams.hs
@@ -9,13 +9,16 @@ import qualified Sound.Tidal.Context as T
 lineOfText :: String -> Diagram B
 lineOfText texttoshow = (alignedText 0 0.5 texttoshow # fontSize eventLabelSize) <> (strut (0.0 ^& 0.03))
 
-leftPlusExample1 :: Diagram B
-leftPlusExample1 =
+patternAlgebraDiagram :: (T.Pattern Double -> T.Pattern Double -> T.Pattern Double) -> String -> T.Pattern Double -> T.Pattern Double -> Diagram B
+patternAlgebraDiagram operator operatorString pat1 pat2 =
     vsep linearDiagramVerticalPadding [
-        patternDiagramLinearWithDoubles (T.parseBP_E "1 2 3" :: T.Pattern Double) 1
-        ,lineOfText "(|+)"
-        ,patternDiagramLinearWithDoubles (T.parseBP_E "20 40" :: T.Pattern Double) 1
+        patternDiagramLinearWithDoubles (pat1) 1
+        ,lineOfText operatorString
+        ,patternDiagramLinearWithDoubles (pat2) 1
         ,lineOfText "=="
-        ,patternDiagramLinearWithDoubles ((T.parseBP_E "1 2 3" :: T.Pattern Double) T.|+ (T.parseBP_E "20 40" :: T.Pattern Double)) 1
+        ,patternDiagramLinearWithDoubles (operator pat1 pat2) 1
         ]
+
+leftPlusExample1 :: Diagram B
+leftPlusExample1 = patternAlgebraDiagram (T.|+) "|+" (T.parseBP_E "1 2 3") (T.parseBP_E "20 40")
 
