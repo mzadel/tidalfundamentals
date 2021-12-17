15599bb1438a35565d90f9d2bfe41340cca9d315
Mark Zadel <mzadel@users.noreply.github.com>
2021-12-17 06:21:23 -0500

    Pass the pattern and query arc into QueryDiagrams.example

--- QueryDiagrams.hs
+++ QueryDiagrams.hs
@@ -7,14 +7,11 @@ import Diagrams.Prelude
 import Diagrams.Backend.SVG.CmdLine
 import qualified Sound.Tidal.Context as T
 
-example :: (T.Event Char -> Int) -> Diagram B
-example colourCharsFunc =
+example :: T.Pattern Char -> T.Arc -> (T.Event Char -> Int) -> Diagram B
+example pat arctoquery colourCharsFunc =
     vsep linearDiagramVerticalPadding [
         Lin.diagramShowCharValue pat 1 1 colourCharsFunc
         ,Lin.arcDiagram [arctoquery]
         ,Lin.queryDiagram (T.queryArc pat arctoquery) colourCharsFunc
         ]
-    where
-        pat = T.fromMaybes [Just 'a',Nothing,Just 'c']
-        arctoquery = T.Arc 0.25 0.75
 
