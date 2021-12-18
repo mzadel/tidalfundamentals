729a3c939af1fc0cf454441e79e35f486b19385b
Mark Zadel <mzadel@users.noreply.github.com>
2021-12-18 12:47:17 -0500

    Add some text to QueryDiagrams example

--- QueryDiagrams.hs
+++ QueryDiagrams.hs
@@ -1,7 +1,7 @@
 
 module QueryDiagrams (example) where
 
-import Shared (linearDiagramVerticalPadding)
+import Shared (linearDiagramVerticalPadding,lineOfText)
 import qualified LinearDiagrams.LinearDiagrams as Lin (diagramShowCharValue)
 import qualified LinearDiagrams.Arc as Lin (arcDiagram)
 import qualified LinearDiagrams.Query as Q (queryDiagram)
@@ -13,7 +13,9 @@ example :: T.Pattern Char -> T.Arc -> (T.Event Char -> Int) -> Diagram B
 example pat arctoquery colourCharsFunc =
     vsep linearDiagramVerticalPadding [
         Lin.diagramShowCharValue pat 1 1 colourCharsFunc
+        ,lineOfText "Arc to query:"
         ,Lin.arcDiagram [arctoquery]
+        ,lineOfText "Result:"
         ,Q.queryDiagram (T.queryArc pat arctoquery) colourCharsFunc
         ]
 
