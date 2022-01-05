0a55b14425636ae2b3b041314f9c714bbe4afd23
Mark Zadel <mzadel@users.noreply.github.com>
2022-01-05 11:29:06 +0100

    Factor out frameAndScale

--- DiagramTable.hs
+++ DiagramTable.hs
@@ -14,8 +14,8 @@ import Diagrams.Backend.SVG.CmdLine
 import qualified Sound.Tidal.Context as T
 import Data.Map ((!))
 
-outputScaling :: Double
-outputScaling = 600
+frameAndScale :: Diagram B -> Diagram B
+frameAndScale diagram = diagram # frame 0.05 # scale 600
 
 colourStringsFunc :: T.Event String -> Int
 colourStringsFunc e = colour
@@ -86,49 +86,49 @@ laneFunc2 e = lane
 basicsTable :: [(String, Diagram B)]
 basicsTable = [
     ("purecycle"
-    ,(Lin.diagramShowValue (PE.purecycleExpr :: T.Pattern String) 1 3 (constColour 0)) # frame 0.05 # scale outputScaling)
+    ,(Lin.diagramShowValue (PE.purecycleExpr :: T.Pattern String) 1 3 (constColour 0)) # frameAndScale)
     ,("fromListExample"
-    ,(Lin.diagramShowValue PE.fromListExampleExpr 1 3 colourStringsFunc) # frame 0.05 # scale outputScaling)
+    ,(Lin.diagramShowValue PE.fromListExampleExpr 1 3 colourStringsFunc) # frameAndScale)
     ,("fastFromListExample"
-    ,(Lin.diagramShowValue PE.fastFromListExampleExpr 3 2 colourStringsFunc) # frame 0.05 # scale outputScaling)
+    ,(Lin.diagramShowValue PE.fastFromListExampleExpr 3 2 colourStringsFunc) # frameAndScale)
     ,("fastFromListExampleCircular"
-    ,(Cir.diagramShowValue PE.fastFromListExampleCircularExpr 3 colourStringsFunc) # frame 0.05 # scale outputScaling)
+    ,(Cir.diagramShowValue PE.fastFromListExampleCircularExpr 3 colourStringsFunc) # frameAndScale)
     ,("fromMaybesExample"
-    ,(Cir.diagramShowValue PE.fromMaybesExampleExpr 3 colourStringsFunc) # frame 0.05 # scale outputScaling)
+    ,(Cir.diagramShowValue PE.fromMaybesExampleExpr 3 colourStringsFunc) # frameAndScale)
     ,("appendExample"
-    ,(Lin.diagramShowValue PE.appendExampleExpr 1 4 colourCharsFunc) # frame 0.05 # scale outputScaling)
+    ,(Lin.diagramShowValue PE.appendExampleExpr 1 4 colourCharsFunc) # frameAndScale)
     ,("catExample"
-    ,(Lin.diagramShowValue PE.catExampleExpr 1 6 colourCharsFunc) # frame 0.05 # scale outputScaling)
+    ,(Lin.diagramShowValue PE.catExampleExpr 1 6 colourCharsFunc) # frameAndScale)
     ,("fastCatExample"
-    ,(Lin.diagramShowValue PE.fastCatExampleExpr 3 2 colourCharsFunc) # frame 0.05 # scale outputScaling)
+    ,(Lin.diagramShowValue PE.fastCatExampleExpr 3 2 colourCharsFunc) # frameAndScale)
     ,("timeCatExample"
-    ,(Lin.diagramShowValue PE.timeCatExampleExpr 3 1 colourCharsFunc) # frame 0.05 # scale outputScaling)
+    ,(Lin.diagramShowValue PE.timeCatExampleExpr 3 1 colourCharsFunc) # frameAndScale)
     ,("overlayExample"
-    ,(Lin.diagramWithLanesShowValue PE.overlayExampleExpr 4 1 laneCharsFunc colourCharsFunc) # frame 0.05 # scale outputScaling)
+    ,(Lin.diagramWithLanesShowValue PE.overlayExampleExpr 4 1 laneCharsFunc colourCharsFunc) # frameAndScale)
     ,("stackExample"
-    ,(Lin.diagramWithLanesShowValue PE.stackExampleExpr 3 2 laneCharsFunc colourCharsFunc) # frame 0.05 # scale outputScaling)
+    ,(Lin.diagramWithLanesShowValue PE.stackExampleExpr 3 2 laneCharsFunc colourCharsFunc) # frameAndScale)
     ,("fastfunction"
-    ,(Lin.diagramShowCharValue PE.fastfunctionExpr 3 1 colourCharsFunc) # frame 0.05 # scale outputScaling)
+    ,(Lin.diagramShowCharValue PE.fastfunctionExpr 3 1 colourCharsFunc) # frameAndScale)
     ,("slowfunction"
-    ,(Lin.diagramShowCharValue PE.slowfunctionExpr 2 3 colourCharsFunc) # frame 0.05 # scale outputScaling)
+    ,(Lin.diagramShowCharValue PE.slowfunctionExpr 2 3 colourCharsFunc) # frameAndScale)
     ,("fastgapfunction"
-    ,(Lin.diagramShowCharValue PE.fastgapfunctionExpr 3 2 colourCharsFunc) # frame 0.05 # scale outputScaling)
+    ,(Lin.diagramShowCharValue PE.fastgapfunctionExpr 3 2 colourCharsFunc) # frameAndScale)
     ,("compressfunction"
-    ,(Lin.diagramShowCharValue PE.compressfunctionExpr 4 3 colourCharsFunc) # frame 0.05 # scale outputScaling)
+    ,(Lin.diagramShowCharValue PE.compressfunctionExpr 4 3 colourCharsFunc) # frameAndScale)
     ,("zoomfunction"
-    ,(Lin.diagramShowCharValue PE.zoomfunctionExpr 6 2 colourCharsFunc) # frame 0.05 # scale outputScaling)
+    ,(Lin.diagramShowCharValue PE.zoomfunctionExpr 6 2 colourCharsFunc) # frameAndScale)
     ,("revfunctioninput"
-    ,(Lin.diagramShowCharValue PE.revfunctioninputExpr 3 3 colourCharsFunc) # frame 0.05 # scale outputScaling)
+    ,(Lin.diagramShowCharValue PE.revfunctioninputExpr 3 3 colourCharsFunc) # frameAndScale)
     ,("revfunctionoutput"
-    ,(Lin.diagramShowCharValue PE.revfunctionoutputExpr 3 3 colourCharsFunc) # frame 0.05 # scale outputScaling)
+    ,(Lin.diagramShowCharValue PE.revfunctionoutputExpr 3 3 colourCharsFunc) # frameAndScale)
     ,("everyfunction"
-    ,(Lin.diagramShowCharValue PE.everyfunctionExpr 1 4 colourCharsFunc) # frame 0.05 # scale outputScaling)
+    ,(Lin.diagramShowCharValue PE.everyfunctionExpr 1 4 colourCharsFunc) # frameAndScale)
     ,("whenfunction"
-    ,(Lin.diagramShowCharValue PE.whenfunctionExpr 1 4 colourCharsFunc) # frame 0.05 # scale outputScaling)
+    ,(Lin.diagramShowCharValue PE.whenfunctionExpr 1 4 colourCharsFunc) # frameAndScale)
     ,("rotRexample"
-    ,(Lin.diagramShowCharValue PE.rotRexampleExpr 1 2 colourCharsFunc) # frame 0.05 # scale outputScaling)
+    ,(Lin.diagramShowCharValue PE.rotRexampleExpr 1 2 colourCharsFunc) # frameAndScale)
     ,("rotRwithfastGap"
-    ,(Lin.diagramShowCharValue PE.rotRwithfastGapExpr 8 1 colourCharsFunc) # frame 0.05 # scale outputScaling)
+    ,(Lin.diagramShowCharValue PE.rotRwithfastGapExpr 8 1 colourCharsFunc) # frameAndScale)
     ]
 
 patternTable :: [(String, (T.Pattern Char, Integer))]
@@ -146,85 +146,85 @@ patternTable = [
 diagramEntry :: (String, (T.Pattern Char, Integer)) -> (String, Diagram B)
 diagramEntry (label, (pat, numticks)) = (label, diagram)
     where
-        diagram = Cir.diagramShowCharValue pat numticks colourCharsFunc # frame 0.05 # scale outputScaling
+        diagram = Cir.diagramShowCharValue pat numticks colourCharsFunc # frameAndScale
 
 diagramTableLinear :: [(String, Diagram B)]
 diagramTableLinear = [
     ("commaforparallel"
-    ,(Lin.diagramWithLanesShowChar (T.parseBP_E PE.commaforparallelExpr :: T.Pattern Char) 4 1 laneFunc colourCharsFunc) # frame 0.05 # scale outputScaling)
+    ,(Lin.diagramWithLanesShowChar (T.parseBP_E PE.commaforparallelExpr :: T.Pattern Char) 4 1 laneFunc colourCharsFunc) # frameAndScale)
     ,("polymetricbraces"
-    ,(Lin.diagramWithLanesShowChar (T.parseBP_E PE.polymetricbracesExpr :: T.Pattern Char) 3 3 laneFunc colourCharsFunc) # frame 0.05 # scale outputScaling)
+    ,(Lin.diagramWithLanesShowChar (T.parseBP_E PE.polymetricbracesExpr :: T.Pattern Char) 3 3 laneFunc colourCharsFunc) # frameAndScale)
     ,("polymetricbracesotherorder"
-    ,(Lin.diagramWithLanesShowChar (T.parseBP_E PE.polymetricbracesotherorderExpr :: T.Pattern Char) 2 3 laneFunc2 colourCharsFunc) # frame 0.05 # scale outputScaling)
+    ,(Lin.diagramWithLanesShowChar (T.parseBP_E PE.polymetricbracesotherorderExpr :: T.Pattern Char) 2 3 laneFunc2 colourCharsFunc) # frameAndScale)
     ,("polymetricdividebyeight"
-    ,(Lin.diagramShowCharValue (T.parseBP_E PE.polymetricdividebyeightExpr :: T.Pattern Char) 8 2 colourCharsFunc) # frame 0.05 # scale outputScaling)
+    ,(Lin.diagramShowCharValue (T.parseBP_E PE.polymetricdividebyeightExpr :: T.Pattern Char) 8 2 colourCharsFunc) # frameAndScale)
     ,("polymetricdividebyseven"
-    ,(Lin.diagramShowCharValue (T.parseBP_E PE.polymetricdividebysevenExpr :: T.Pattern Char) 7 2 colourCharsFunc) # frame 0.05 # scale outputScaling)
+    ,(Lin.diagramShowCharValue (T.parseBP_E PE.polymetricdividebysevenExpr :: T.Pattern Char) 7 2 colourCharsFunc) # frameAndScale)
     ,("anglebrackets"
-    ,(Lin.diagramShowCharValue (T.parseBP_E PE.anglebracketsExpr :: T.Pattern Char) 3 4 colourCharsFunc) # frame 0.05 # scale outputScaling)
+    ,(Lin.diagramShowCharValue (T.parseBP_E PE.anglebracketsExpr :: T.Pattern Char) 3 4 colourCharsFunc) # frameAndScale)
     ,("euclideanrhythm"
-    ,(Lin.diagramShowCharValue (T.parseBP_E PE.euclideanrhythmExpr :: T.Pattern Char) 7 2 colourCharsFunc) # frame 0.05 # scale outputScaling)
+    ,(Lin.diagramShowCharValue (T.parseBP_E PE.euclideanrhythmExpr :: T.Pattern Char) 7 2 colourCharsFunc) # frameAndScale)
     ,("slowoneandahalf"
-    ,(Lin.diagramShowCharValue PE.slowoneandahalfExpr 2 3 colourCharsFunc) # frame 0.05 # scale outputScaling)
+    ,(Lin.diagramShowCharValue PE.slowoneandahalfExpr 2 3 colourCharsFunc) # frameAndScale)
     ]
 
 diagramTablePatternAlgebra :: [(String, Diagram B)]
 diagramTablePatternAlgebra = [
-    ("numberpatternmin", Lin.diagramFromWholes show (PE.numberpatternminExpr :: T.Pattern Int) 1 # frame 0.05 # scale outputScaling)
-    ,("numberpatternmax", Lin.diagramFromWholes show (PE.numberpatternmaxExpr :: T.Pattern Int) 1 # frame 0.05 # scale outputScaling)
-    ,("numberpatternplus", Lin.diagramFromWholes show (PE.numberpatternplusExpr :: T.Pattern Int) 1 # frame 0.05 # scale outputScaling)
-    ,("numberpatterntimes", Lin.diagramFromWholes show (PE.numberpatterntimesExpr :: T.Pattern Int) 1 # frame 0.05 # scale outputScaling)
-    ,("numberpatternmod", Lin.diagramFromWholes show (PE.numberpatternmodExpr :: T.Pattern Int) 1 # frame 0.05 # scale outputScaling)
-    ,("numberpatternsqrt", Lin.diagramFromWholes showDoubleTruncated PE.numberpatternsqrtExpr 1 # frame 0.05 # scale outputScaling)
-    ,("additionexample", PA.additionexample # frame 0.05 # scale outputScaling)
-    ,("leftPlusExample1", PA.leftPlusExample1 # frame 0.05 # scale outputScaling)
-    ,("leftPlusExample2", PA.leftPlusExample2 # frame 0.05 # scale outputScaling)
-    ,("leftPlusExample3", PA.leftPlusExample3 # frame 0.05 # scale outputScaling)
-    ,("rightPlusExample1", PA.rightPlusExample1 # frame 0.05 # scale outputScaling)
-    ,("rightPlusExample2", PA.rightPlusExample2 # frame 0.05 # scale outputScaling)
-    ,("rightPlusExample3", PA.rightPlusExample3 # frame 0.05 # scale outputScaling)
-    ,("bothPlusExample1", PA.bothPlusExample1 # frame 0.05 # scale outputScaling)
-    ,("bothPlusExample2", PA.bothPlusExample2 # frame 0.05 # scale outputScaling)
-    ,("bothPlusExample3", PA.bothPlusExample3 # frame 0.05 # scale outputScaling)
-    ,("justPlusExample1", PA.justPlusExample1 # frame 0.05 # scale outputScaling)
-    ,("applicativeboth", Lin.diagramFromWholes showDoubleTruncated (PE.applicativebothExpr :: T.Pattern Double) 1 # frame 0.05 # scale outputScaling)
-    ,("applicativeleft", Lin.diagramFromWholes showDoubleTruncated (PE.applicativeleftExpr :: T.Pattern Double) 1 # frame 0.05 # scale outputScaling)
-    ,("applicativeright", Lin.diagramFromWholes showDoubleTruncated (PE.applicativerightExpr :: T.Pattern Double) 1 # frame 0.05 # scale outputScaling)
-    ,("filtervaluesexample", Lin.diagramFromWholes show PE.filtervaluesexampleExpr 1 # frame 0.05 # scale outputScaling)
-    ,("valueAlgebraMapDiagram", PA.valueAlgebraMapDiagram # frame 0.05 # scale outputScaling)
+    ("numberpatternmin", Lin.diagramFromWholes show (PE.numberpatternminExpr :: T.Pattern Int) 1 # frameAndScale)
+    ,("numberpatternmax", Lin.diagramFromWholes show (PE.numberpatternmaxExpr :: T.Pattern Int) 1 # frameAndScale)
+    ,("numberpatternplus", Lin.diagramFromWholes show (PE.numberpatternplusExpr :: T.Pattern Int) 1 # frameAndScale)
+    ,("numberpatterntimes", Lin.diagramFromWholes show (PE.numberpatterntimesExpr :: T.Pattern Int) 1 # frameAndScale)
+    ,("numberpatternmod", Lin.diagramFromWholes show (PE.numberpatternmodExpr :: T.Pattern Int) 1 # frameAndScale)
+    ,("numberpatternsqrt", Lin.diagramFromWholes showDoubleTruncated PE.numberpatternsqrtExpr 1 # frameAndScale)
+    ,("additionexample", PA.additionexample # frameAndScale)
+    ,("leftPlusExample1", PA.leftPlusExample1 # frameAndScale)
+    ,("leftPlusExample2", PA.leftPlusExample2 # frameAndScale)
+    ,("leftPlusExample3", PA.leftPlusExample3 # frameAndScale)
+    ,("rightPlusExample1", PA.rightPlusExample1 # frameAndScale)
+    ,("rightPlusExample2", PA.rightPlusExample2 # frameAndScale)
+    ,("rightPlusExample3", PA.rightPlusExample3 # frameAndScale)
+    ,("bothPlusExample1", PA.bothPlusExample1 # frameAndScale)
+    ,("bothPlusExample2", PA.bothPlusExample2 # frameAndScale)
+    ,("bothPlusExample3", PA.bothPlusExample3 # frameAndScale)
+    ,("justPlusExample1", PA.justPlusExample1 # frameAndScale)
+    ,("applicativeboth", Lin.diagramFromWholes showDoubleTruncated (PE.applicativebothExpr :: T.Pattern Double) 1 # frameAndScale)
+    ,("applicativeleft", Lin.diagramFromWholes showDoubleTruncated (PE.applicativeleftExpr :: T.Pattern Double) 1 # frameAndScale)
+    ,("applicativeright", Lin.diagramFromWholes showDoubleTruncated (PE.applicativerightExpr :: T.Pattern Double) 1 # frameAndScale)
+    ,("filtervaluesexample", Lin.diagramFromWholes show PE.filtervaluesexampleExpr 1 # frameAndScale)
+    ,("valueAlgebraMapDiagram", PA.valueAlgebraMapDiagram # frameAndScale)
     ]
 
 diagramTableSignals :: [(String, Diagram B)]
 diagramTableSignals = [
-    ("sigEvaluatesAtMiddle", Sig.sigEvaluatesAtMiddle # frame 0.05 # scale outputScaling)
-    ,("sigToSetPanning", Sig.sigToSetPanning # frame 0.05 # scale outputScaling)
+    ("sigEvaluatesAtMiddle", Sig.sigEvaluatesAtMiddle # frameAndScale)
+    ,("sigToSetPanning", Sig.sigToSetPanning # frameAndScale)
     ]
 
 diagramTableQueries :: [(String, Diagram B)]
 diagramTableQueries = [
-    ("queryFullCycle", Q.example PE.queryFullCyclePatternExpr PE.queryFullCycleArcExpr colourCharsFunc # frame 0.05 # scale outputScaling)
-    ,("queryHalfCycle", Q.example PE.queryHalfCyclePatternExpr PE.queryHalfCycleArcExpr colourCharsFunc # frame 0.05 # scale outputScaling)
-    ,("queryQuarterCycle", Q.example PE.queryQuarterCyclePatternExpr PE.queryQuarterCycleArcExpr colourCharsFunc # frame 0.05 # scale outputScaling)
-    ,("queryEmptySpace", Q.example PE.queryEmptySpacePatternExpr PE.queryEmptySpaceArcExpr colourCharsFunc # frame 0.05 # scale outputScaling)
-    ,("queryOverlapOneItemExactly", Q.example PE.queryOverlapOneItemExactlyPatternExpr PE.queryOverlapOneItemExactlyArcExpr colourCharsFunc # frame 0.05 # scale outputScaling)
-    ,("queryOverlapOneItemExactly2", Q.example PE.queryOverlapOneItemExactly2PatternExpr PE.queryOverlapOneItemExactly2ArcExpr colourCharsFunc # frame 0.05 # scale outputScaling)
-    ,("queryOverlapTwoItemsExactly", Q.example PE.queryOverlapTwoItemsExactlyPatternExpr PE.queryOverlapTwoItemsExactlyArcExpr colourCharsFunc # frame 0.05 # scale outputScaling)
-    ,("queryOverlapTwoItems", Q.example PE.queryOverlapTwoItemsPatternExpr PE.queryOverlapTwoItemsArcExpr colourCharsFunc # frame 0.05 # scale outputScaling)
-    ,("queryTwoItemsZeroWidth", Q.example PE.queryTwoItemsZeroWidthPatternExpr PE.queryTwoItemsZeroWidthArcExpr colourCharsFunc # frame 0.05 # scale outputScaling)
-    ,("queryOneItemZeroWidth", Q.example PE.queryOneItemZeroWidthPatternExpr PE.queryOneItemZeroWidthArcExpr colourCharsFunc # frame 0.05 # scale outputScaling)
-    ,("simplesine", Curve.curveDiagram PE.simplesineExpr 1 # frame 0.05 # scale outputScaling)
-    ,("querycontinuouspattern", Q.continuousPatternExample PE.querycontinuouspatternPatternExpr PE.querycontinuouspatternArcExpr # frame 0.05 # scale outputScaling)
-    ,("querycontinuouspatternshowpoint", Q.continuousPatternExampleShowPoint PE.querycontinuouspatternshowpointPatternExpr PE.querycontinuouspatternshowpointArcExpr # frame 0.05 # scale outputScaling)
-    ,("querycontinuouspatternsamplesatmidpoint", Q.continuousPatternExampleShowPoint PE.querycontinuouspatternsamplesatmidpointPatternExpr PE.querycontinuouspatternsamplesatmidpointArcExpr # frame 0.05 # scale outputScaling)
-    ,("querycontinuouspatternzwarc", Q.continuousPatternExampleShowPoint PE.querycontinuouspatternzwarcPatternExpr PE.querycontinuouspatternzwarcArcExpr # frame 0.05 # scale outputScaling)
+    ("queryFullCycle", Q.example PE.queryFullCyclePatternExpr PE.queryFullCycleArcExpr colourCharsFunc # frameAndScale)
+    ,("queryHalfCycle", Q.example PE.queryHalfCyclePatternExpr PE.queryHalfCycleArcExpr colourCharsFunc # frameAndScale)
+    ,("queryQuarterCycle", Q.example PE.queryQuarterCyclePatternExpr PE.queryQuarterCycleArcExpr colourCharsFunc # frameAndScale)
+    ,("queryEmptySpace", Q.example PE.queryEmptySpacePatternExpr PE.queryEmptySpaceArcExpr colourCharsFunc # frameAndScale)
+    ,("queryOverlapOneItemExactly", Q.example PE.queryOverlapOneItemExactlyPatternExpr PE.queryOverlapOneItemExactlyArcExpr colourCharsFunc # frameAndScale)
+    ,("queryOverlapOneItemExactly2", Q.example PE.queryOverlapOneItemExactly2PatternExpr PE.queryOverlapOneItemExactly2ArcExpr colourCharsFunc # frameAndScale)
+    ,("queryOverlapTwoItemsExactly", Q.example PE.queryOverlapTwoItemsExactlyPatternExpr PE.queryOverlapTwoItemsExactlyArcExpr colourCharsFunc # frameAndScale)
+    ,("queryOverlapTwoItems", Q.example PE.queryOverlapTwoItemsPatternExpr PE.queryOverlapTwoItemsArcExpr colourCharsFunc # frameAndScale)
+    ,("queryTwoItemsZeroWidth", Q.example PE.queryTwoItemsZeroWidthPatternExpr PE.queryTwoItemsZeroWidthArcExpr colourCharsFunc # frameAndScale)
+    ,("queryOneItemZeroWidth", Q.example PE.queryOneItemZeroWidthPatternExpr PE.queryOneItemZeroWidthArcExpr colourCharsFunc # frameAndScale)
+    ,("simplesine", Curve.curveDiagram PE.simplesineExpr 1 # frameAndScale)
+    ,("querycontinuouspattern", Q.continuousPatternExample PE.querycontinuouspatternPatternExpr PE.querycontinuouspatternArcExpr # frameAndScale)
+    ,("querycontinuouspatternshowpoint", Q.continuousPatternExampleShowPoint PE.querycontinuouspatternshowpointPatternExpr PE.querycontinuouspatternshowpointArcExpr # frameAndScale)
+    ,("querycontinuouspatternsamplesatmidpoint", Q.continuousPatternExampleShowPoint PE.querycontinuouspatternsamplesatmidpointPatternExpr PE.querycontinuouspatternsamplesatmidpointArcExpr # frameAndScale)
+    ,("querycontinuouspatternzwarc", Q.continuousPatternExampleShowPoint PE.querycontinuouspatternzwarcPatternExpr PE.querycontinuouspatternzwarcArcExpr # frameAndScale)
     ]
 
 diagramTableJoins :: [(String, Diagram B)]
 diagramTableJoins = [
-    ("unwrapexample", Lin.diagramShowCharValue PE.unwrapexampleExpr 6 1 colourCharsFunc # frame 0.05 # scale outputScaling)
-    ,("innerjoinexample", Lin.diagramShowCharValue PE.innerjoinexampleExpr 6 1 colourCharsFunc # frame 0.05 # scale outputScaling)
-    ,("outerjoinexample", Lin.diagramShowCharValue PE.outerjoinexampleExpr 2 1 colourCharsFunc # frame 0.05 # scale outputScaling)
-    ,("squeezejoinexample", Lin.diagramShowCharValue PE.squeezejoinexampleExpr 2 1 colourCharsFunc # frame 0.05 # scale outputScaling)
+    ("unwrapexample", Lin.diagramShowCharValue PE.unwrapexampleExpr 6 1 colourCharsFunc # frameAndScale)
+    ,("innerjoinexample", Lin.diagramShowCharValue PE.innerjoinexampleExpr 6 1 colourCharsFunc # frameAndScale)
+    ,("outerjoinexample", Lin.diagramShowCharValue PE.outerjoinexampleExpr 2 1 colourCharsFunc # frameAndScale)
+    ,("squeezejoinexample", Lin.diagramShowCharValue PE.squeezejoinexampleExpr 2 1 colourCharsFunc # frameAndScale)
     ]
 
 lanesFromPanning :: T.Event T.ValueMap -> Int
@@ -235,8 +235,8 @@ lanesFromPanning e = case T.fvalue (T.value e ! "pan") of
 
 diagramTableUI :: [(String, Diagram B)]
 diagramTableUI = [
-    ("juxexample", Lin.diagramWithLanesFromWholes show PE.juxexampleExpr 1 lanesFromPanning # frame 0.05 # scale outputScaling)
-    ,("shuffleexample", Lin.diagramFromWholes show PE.shuffleexampleExpr 3 # frame 0.05 # scale outputScaling)
+    ("juxexample", Lin.diagramWithLanesFromWholes show PE.juxexampleExpr 1 lanesFromPanning # frameAndScale)
+    ,("shuffleexample", Lin.diagramFromWholes show PE.shuffleexampleExpr 3 # frameAndScale)
     ]
 
 diagramListForMainWith :: [(String, Diagram B)]
